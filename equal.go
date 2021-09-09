ec7f7f399534243854608e3333dc34e4e113f05f
Maxim Oransky <maxim.oransky@gmail.com>
2021-09-09 11:02:01 +0300

    StringEqual, BytesEqual

--- equal.go
+++ equal.go
@@ -1,6 +1,9 @@
 package ref
 
-import "time"
+import (
+	"bytes"
+	"time"
+)
 
 func IntEqual(v1, v2 *int) bool {
 	if v1 == nil && v2 == nil {
@@ -30,4 +33,24 @@ func TimeEqual(v1, v2 *time.Time) bool {
 		return false
 	}
 	return v1.Equal(*v2)
+}
+
+func StringEqual(v1, v2 *string) bool {
+	if v1 == nil && v2 == nil {
+		return true
+	}
+	if v1 == nil || v2 == nil {
+		return false
+	}
+	return *v1 == *v2
+}
+
+func BytesEqual(v1, v2 *[]byte) bool {
+	if v1 == nil && v2 == nil {
+		return true
+	}
+	if v1 == nil || v2 == nil {
+		return false
+	}
+	return bytes.Equal(*v1, *v2)
 }
